1fbf42b2c968d8e1440892d40a09e0f5a58ce02a
GMartins <guilherme.martinst.dev@gmail.com>
2023-09-20 17:20:09 -0300

    Refactor: main file with server connection, routes and database

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("URL shortener with Node.js and Express");
-});
+// Import database config
+require("./db/db");
+
+// Config to JSON
+app.use(express.json());
+
+// Import routes
+const urlRoutes = require("./routes/routes");
+app.use("/urls", urlRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
